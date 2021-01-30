e2e9919821fee51cbacb3d89850b8cdaee759076
mahmoud ali <mahmoudali009989@gmail.com>
2021-01-30 14:43:18 +0200

    =user login

--- routes/site.php
+++ routes/site.php
@@ -2,8 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| site Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 
-Route::get('/',function(){
+
+route::get('/',function(){
     return view('front.home');
+}) -> name('home');
+
+
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+], function () {
+
+    Route::group(['namespace' => 'Site', 'middleware' => 'auth'], function () {
+        // must be authenticated user
+    });
+
+    Route::group(['namespace' => 'Site', 'middleware' => 'guest'], function () {
+
+        //guest  user
+
+
+
+    });
+
 });
