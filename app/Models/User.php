c4b1bd39ba59b3968a848903e8977b86c57e8460
mahmoud ali <mahmoudali009989@gmail.com>
2021-02-22 14:57:12 +0200

    wishlist

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,19 @@ class User extends Authenticatable
     public function codes(){
         return $this->hasMany(User_verfication::class,'user_id');
     }
+
+
+    /**
+     * get the wishlist of the user.
+     *
+     *
+     */
+    public function wishlist(){
+        return $this->belongsToMany(Product::class,'wish_lists')->withTimestamps();
+    }
+
+
+    public function wishlistHas($productId){
+        return self::wishlist()->where('product_id',$productId)->exists();
+    }
 }
