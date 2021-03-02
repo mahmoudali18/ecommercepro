78fed8dcc39d3ceac732e07f3ad734ea5e2afecc
mahmoud ali <mahmoudali009989@gmail.com>
2021-03-02 14:58:52 +0200

    end

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Astrotomic\Translatable\Translatable;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
@@ -12,9 +10,9 @@ class Slider extends Model
     protected $fillable = ['photo', 'created_at', 'updated_at'];
 
 
-    public function getPhotoAttribute($val)
-    {
-        return ($val !== null) ? asset('assets/images/sliders/' . $val) : "";
-    }
 
+
+    public function getPhotoAttribute($val) {
+        return ($val !== null) ? asset('assets/images/sliders/' . $val) : '';
+    }
 }
